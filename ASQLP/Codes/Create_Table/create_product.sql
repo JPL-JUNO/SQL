9d2c9caf20e191653bae1acf0d700141fd54f3b3
Stephen CUI <junocuixuan@163.com>
2023-05-09 22:49:28 +0800

    自连接

---
+++ ASQLP/Codes/Create_Table/create_product.sql
@@ -0,0 +1,13 @@
+CREATE TABLE
+    Product (
+        name VARCHAR(16) PRIMARY KEY,
+        price INTEGER NOT NULL
+    );
+
+DELETE FROM Product;
+
+INSERT INTO Product VALUES('苹果', 100);
+
+INSERT INTO Product VALUES('橘子', 50);
+
+INSERT INTO Product VALUES('香蕉', 80);
