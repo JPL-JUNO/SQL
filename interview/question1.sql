0e77c238e4d769695a4fe3b3a0c1fab52d0e2c68
sunyardCUI <1924375084@qq.com>
2023-07-14 17:17:41 +0800

    在A主键中但不在B主键中

--- interview/question1.sql
+++ interview/question1.sql
@@ -4,3 +4,10 @@ SELECT A.primary_key
 FROM A
     LEFT JOIN B ON B.primary_key = A.primary_key -- LEFT JOIN B USING(primary_key)
 WHERE B.primary_key IS NULL;
+
+SELECT primary_key
+FROM A
+WHERE primary_key NOT IN (
+        SELECT primary_key
+        FROM B
+    );
